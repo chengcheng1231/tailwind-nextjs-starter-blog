6516fbd171b01c14722f900769e81f88ad092921
yucheng <yuchenghsu@gmail.com>
2022-08-13 20:56:29 +0800

    feat: change logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,6 +17,7 @@ const siteMetadata = {
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
+  logoImage: '/static/images/logo-new.png',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
